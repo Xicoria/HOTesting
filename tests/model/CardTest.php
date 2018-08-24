33c2dbea93b6a9ff2d34a28ca027bca7a1e56d6f
Francisco Camargos <kxdevel@gmail.com>
2018-08-24 23:17:48 +0100

    Adding setup to initialize common used fixture

--- tests/model/CardTest.php
+++ tests/model/CardTest.php
@@ -4,31 +4,35 @@ use HOTesting\Model\Card;
 
 class CardTest extends \PHPUnit\Framework\TestCase
 {
+    private $card;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->card = new Card('4', 'S');
+    }
+
     public function testGetNumber()
     {
-        $card = new Card('4', 'S');
-        $actualNumber = $card->getNumber();
+        $actualNumber = $this->card->getNumber();
         $this->assertEquals('4', $actualNumber, 'The number should be <4>');
     }
 
     public function testSuit()
     {
-        $card = new Card('4', 'S');
-        $actualSuit = $card->getSuit();
+        $actualSuit = $this->card->getSuit();
         $this->assertEquals('S', $actualSuit, 'The suit should be <S>');
     }
 
     public function testIsInMatchingSet()
     {
-        $card = new Card('4', 'S');
         $matchingCard = new Card('4', 'H');
-        $this->assertTrue($card->isInMatchingSet($matchingCard), '<4S> should match <4H>');
+        $this->assertTrue($this->card->isInMatchingSet($matchingCard), '<4S> should match <4H>');
     }
 
     public function testIsNotInMatchingSet()
     {
-        $card = new Card('4', 'S');
         $matchingCard = new Card('5', 'H');
-        $this->assertFalse($card->isInMatchingSet($matchingCard), '<4S> should not match <5H>');
+        $this->assertFalse($this->card->isInMatchingSet($matchingCard), '<4S> should not match <5H>');
     }
 }
